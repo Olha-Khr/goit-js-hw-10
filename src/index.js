648d7d0318120cf7e82105b2b2ad8d9f4470a119
Olha <99271539+Olha-Khr@users.noreply.github.com>
2022-08-29 03:59:44 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    container: document.querySelector('.country-info')
+}
+
+export { refs };
+
+const onInput = (e) => {
+    if (!e.target.value) {
+        return refs.list.innerHTML = '';
+    }
+    fetchCountries(e.target.value.trim())
+}
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
